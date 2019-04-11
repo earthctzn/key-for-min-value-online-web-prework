d6379f7b358f3b3d3d485956f4d785197c149a5a
Caleb Einarr <earthctzn1@gmail.com>
2019-04-11 02:52:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil 
-else hash = name_hash.sort_by{|key, value| value} 
+  else hash = name_hash.sort_by{|key, value| value} 
   minval_hash=hash[0]
  minval_hash[0]
 end
